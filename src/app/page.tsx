a859ce7630c48d616ea4ce428fe287ced282442d
Benoit GRASSET <b.grasset@cliqdigital.com>
2024-03-06 16:07:32 +0100

    add carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,14 @@
 
 import MovieCard from "@/components/MovieCard";
 import { getMovies } from "@/shared/api";
-import Flicking from "@egjs/react-flicking";
+import { Arrow } from "@egjs/flicking-plugins";
+import "@egjs/flicking-plugins/dist/arrow.css";
+import Flicking, { ViewportSlot } from "@egjs/react-flicking";
 import "@egjs/react-flicking/dist/flicking.css";
 import { useEffect, useState } from "react";
 import styles from "./page.module.scss";
+
+const _plugins = [new Arrow()];
 
 export default function Home() {
   /**
@@ -27,16 +31,21 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.carousel}>
         <h2>{`${movies.length} most popular movies`}</h2>
-        <Flicking align="prev" circular={true} useFindDOMNode>
+        <Flicking
+          align="prev"
+          circular={true}
+          useFindDOMNode
+          plugins={_plugins}
+        >
           {movies.map((movie) => {
             return (
-              <MovieCard
-                key={movie.id}
-                movie={movie}
-                className={styles.card2}
-              />
+              <MovieCard key={movie.id} movie={movie} className={styles.card} />
             );
           })}
+          <ViewportSlot>
+            <span className="flicking-arrow-prev"></span>
+            <span className="flicking-arrow-next"></span>
+          </ViewportSlot>
         </Flicking>
       </div>
     </main>
